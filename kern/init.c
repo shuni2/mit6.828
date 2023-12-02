c30cc9aeff538795853f2bd5e1e1def7ee17cb9d
tos0ph <tosoph@outlook.com>
2023-12-02 23:53:06 +0800

    lab4 exercise 5

--- kern/init.c
+++ kern/init.c
@@ -43,7 +43,7 @@ i386_init(void)
 
 	// Acquire the big kernel lock before waking up APs
 	// Your code here:
-
+	lock_kernel();	//获取大内核锁
 	// Starting non-boot CPUs
 	boot_aps();
 
@@ -109,7 +109,8 @@ mp_main(void)
 	// only one CPU can enter the scheduler at a time!
 	//
 	// Your code here:
-
+	lock_kernel();//获取大内核锁
+	sched_yield();//在该AP上运行进程
 	// Remove this after you finish Exercise 6
 	for (;;);
 }
